8df43fe8578f52dacda6cc3ccf4a005ae28f013a
awol17 <alexander.l.wold@gmail.com>
2025-06-06 04:59:24 +0000

    Updated page.tsx to leverage new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,28 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useAdvocatesPage } from "@/app/hooks/useAdvocatesPage";
+import AdvocatesTable from "@/app/components/AdvocatesTable/AdvocatesTable";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import { TopBar } from "./components/TopBar.styles";
+import ErrorBanner from "./components/ErrorBanner";
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
-
-  useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
-      });
-    });
-  }, []);
-
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term").innerHTML = searchTerm;
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
-      );
-    });
-
-    setFilteredAdvocates(filteredAdvocates);
-  };
-
-  const onClick = () => {
-    console.log(advocates);
-    setFilteredAdvocates(advocates);
-  };
+  const { handleSearchChange, handleResetClick, loading, error, searchTerm, advocates, page, totalPages, setPage } = useAdvocatesPage();
 
   return (
     <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
+      <Header />
       <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
-      </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <TopBar>
+        {error && <ErrorBanner message={error?.message} />}
+        <SearchBar
+          searchTerm={searchTerm}
+          onSearchChange={handleSearchChange}
+          onResetClick={handleResetClick}
+        />
+      </TopBar>
+      <AdvocatesTable loading={loading} advocates={advocates} page={page} totalPages={totalPages} setPage={setPage} />
     </main>
   );
 }
